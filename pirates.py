c82c914fd588da165e1cc0fde8c588e7a93f2ead
Philip Harmon <55819132+pharmon9847@users.noreply.github.com>
2022-08-29 22:50:25 -0400

    initial push

---
+++ pirates.py
@@ -0,0 +1,15 @@
+## A feeble attempt at creating a function which returns the number of
+## pirates which form a loop
+
+def my_function(pirates):
+        n = len(pirates)
+        for p in range(0, n - 1):
+            print(p)
+
+    
+barstools = [2, 0, 4, 3, 4, 2, 1]
+    
+my_function(barstools)    
+
+
+    
